27925dcba2c335a7fdfc0c106ecb70689f69190f
Nathan-88 <onwukaebuka88@gmail.com>
2023-11-17 22:32:12 +0100

    remove darkmode

--- components/BlogPostList.vue
+++ components/BlogPostList.vue
@@ -11,7 +11,7 @@ const { data: blogPostList } = await useAsyncData('blogPostList', () => {
     <!-- BLOG POSTS -->
     <div v-for="posts in blogPostList" :key="posts._path" class="w-full  lg:w-
     2/3 md:w-1/2 sm:w-1/1 px-4 py-6 border rounded-lg shadow
-    bg-white dark:bg-gray-800">
+    bg-white">
     <NuxtLink :to="posts._path">
         <h2 class="text-xl font-bold text-black hover:underline p-3 start">{{ posts.title }}</h2>
         <div class=" px-2">

4691853989d25f7944e235769c902049134d89b4
Nathan-88 <onwukaebuka88@gmail.com>
2024-01-23 09:47:58 +0100

    changes to the founders section finalized

--- components/Founders.vue
+++ components/Founders.vue
@@ -2,20 +2,23 @@
   <section class="p-4 lg:p-8">
     <h2 class=" p-4 text-3xl text-center underline decoration-gray-700 decoration-double font-bold pb-4 lg:pb-10  md:text-3xl">The Convener - G.H.A.N</h2>
 
-      <div class="w-[100%] flex flex-col md:flex-row justify-center items-start p-4 md:p-6 md:gap-[5%]">
+      <div class="w-[100%] flex flex-col md:flex-row justify-center items-start p-4 md:p-6 md:gap-[7%]">
 
-        <img class="md:h-[480px] md:w-[40%] rounded-lg obect-cover" src="../assets/images/flora.png" alt="Pst Flora" />
+        <img class="md:w-[40%] rounded-lg obect-cover" src="../assets/images/flora.png" alt="Pst Flora" />
   
         
-        <p class="pt-6 md:pt-0 md:[40%] text-slate-800 text-justify">Articulate, spirit-filled, dexterous, creative, and insightful. A woman of capacity who juggles between tasks for result oriented performance. These are a few words that best describe Pastor Favour Eyimife.
+        <div class="pt-6 md:pt-0 md:[40%] text-slate-800 text-justify">
+          <h2 class="text-4xl md:text-5xl lg:text-6xl text-amber-900 font-bold pb-3">Pastor <br>Flora Eyimife</h2>
+          <p class="pt-3 font-medium">Pastor Flora Eyimife is an articulate, spirit-filled, dexterous, creative and insightful woman of capacity who has distinguished herself in various fields, such as real estate business, interior/exterior designing, remodeling & decoration, public speaking, counseling, and teaching the word of God.</p>
 
-        An intercessor with a deep commitment to prayer, Pastor Flora engages through various mediums to stand in the gap for intercession. She is the Convener of Global Harvest And Network, a vibrant and Christ-Centered
-        community that connects Christians from diverse backgrounds and continents in fellowship.
+        <p class="pt-3 font-medium">She is currently the Executive Officer (CEO) of Crowning Favours Limited, a growing interior/exterior designing, remodeling, and decoration company in the heart of Lagos, Nigeria. Her creativity and pursuit for excellence have led the company to consult on and manage construction projects for individuals, businesses, and multinational organizations.</p>
 
-        She currently serves as the Founder/Chief Executive Officer (CEO) of Crowning Favour Limited, one of the fastest-growing architectural and interior designing and decoration companies in the heart of Lagos, Nigeria. Her creativity and pursuit for excellence have led the company to consult on and manage interior construction projects for individuals, businesses, and multinational organizations. 
+        <p class="pt-3 font-medium">Pastor Flora is an advocate for teens and singles, and the convener of Oasis of Favour Foundation with the responsibility of caring for widows, and the needy while  sponsoring the education and vocational training of many less privileged and out-of-school children, enabling them pursue their dream of completing their tertiary education. </p>
 
-        Pastor Flora is happily married to Pastor Alfred Eyimife, who is the lead pastor of Realm of Glory Church, Ikotun-Egbe Center, where she also serves as Co-Lead Pastor. They are blessed with lovely children."
-        </p>
+        <p class="pt-3 font-medium">She is also the founder of Global Harvest And Network(GHAN), a vibrant and Christ-Centred community that connects Christians from diverse backgrounds and  continents in fellowship, prayer, moral, and spiritual support, born out of her deep desire and  commitment to see young adults grow in faith and their walk with God.</p>
+
+        <p class="pt-2 font-medium">She is happily married and blessed with many children.</p>
+        </div>
       </div>
     
   </section>

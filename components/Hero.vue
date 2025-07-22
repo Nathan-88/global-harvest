7b18576b5921f1b724d65733c629c40726ba314a
Nathan-88 <onwukaebuka88@gmail.com>
2025-07-22 12:09:57 +0100

    updated node from 18.x to 22.x

--- components/Hero.vue
+++ components/Hero.vue
@@ -6,7 +6,7 @@
             <h1 class="text-3xl sm:text-5xl lg:text-7xl font-extrabold mb-4">Welcome to Global <br /> Harvest & Network</h1>
             <p class="text-sm md:text-base md:font-medium mb-6 whitespace-normal md:mt-4 md:mb-10">A vibrant and Christ-Centered community that connect Christians from diverse backgrounds and locations in fellowship, prayer and spiritual support</p>
             <!-- call to action button -->
-            <NuxtLink to="#contact"><button class="hover:bg-yellow-800 text-white text-light md:text-lg md:font-bold p-4 md:px-6 text-sm md:text-lg transition duration-300 transform hover:scale-105 bg-brown animate-bounce rounded-full">Connect with us</button></NuxtLink>
+            <NuxtLink to="#contact"><button class="hover:bg-yellow-800 text-white text-light md:font-bold p-4 md:px-6 text-sm md:text-lg transition duration-300 transform hover:scale-105 bg-brown animate-bounce rounded-full">Connect with us</button></NuxtLink>
         </div>
   </section>
 </template>
